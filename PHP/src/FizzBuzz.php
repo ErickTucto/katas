953adcbcadcc5e842658c009074ac2ae8e4acdc3
Erick Tucto <22831024+ErickTucto@users.noreply.github.com>
2017-11-07 16:58:56 -0500

    Result by @ErickTucto

--- PHP/src/FizzBuzz.php
+++ PHP/src/FizzBuzz.php
@@ -4,5 +4,19 @@ namespace Katas;
 
 class FizzBuzz
 {
-    //
+    /**
+     * @var
+     */
+    protected $result;
+
+    public function number($number)
+    {
+        if ($number % 3 == 0) {
+            $this->result = 'fizz';
+        }
+        if ($number % 5 == 0) {
+            $this->result .= 'buzz';
+        }
+        return ($this->result) ?? $number;
+    }
 }
